c86fc196da4864394b236708622bc7523703e005
Phil Nelan <7775965+nelanphil@users.noreply.github.com>
2025-11-18 07:07:05 -0500

    0.0.0.2.1
    -More Google oAuth vite setting configuration.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,13 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
+const isProduction = process.env.NODE_ENV === 'production';
+const API_URL = isProduction
+  ? 'https://project-surf-server.onrender.com/api'
+  : process.env.VITE_API_URL || 'http://localhost:5050/api';
+
 export default defineConfig({
   plugins: [react()],
   define: {
     // This will be replaced at build time
-    'import.meta.env.VITE_API_URL': JSON.stringify(
-      process.env.VITE_API_URL || 'http://localhost:5050/api',
-    ),
+    'import.meta.env.VITE_API_URL': JSON.stringify(API_URL),
   },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
